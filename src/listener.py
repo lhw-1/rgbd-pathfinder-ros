c1a2c798c2666af2e1ae45f0799424f7e82a6fd2
lhw-1 <leehyungwoonsamuel@gmail.com>
2022-07-11 20:57:30 +0800

    Update script post-testing

--- src/listener.py
+++ src/listener.py
@@ -16,6 +16,8 @@ from cv_bridge import CvBridge, CvBridgeError
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from geometry_msgs.msg import Twist
+from PIL import Image as PILImage
+from PIL import ImageDraw
 
 from detectron2.config import get_cfg
 from detectron2.data.detection_utils import read_image
@@ -29,7 +31,12 @@ WINDOW_NAME = "mask2former demo"
 TRAVERSABLE = [3,11,52,53,59,91,96,121]
 MODEL_DIR = "../data/model_final_5c90d4.pkl"
 CONFIGS_DIR = "../data/maskformer2_R50_bs16_160k.yaml"
-IMAGE_FILENAME = "../data/test.png"
+IMAGE_PATH = "../data/" # Input from camera
+IMAGE_COUNTER = 0
+RGBDP_PATH = "../data/rgbdp_outputs/"
+ANGULARZ = 0
+DEMO = 0
+LOGGER = 0
 
 def generate_mapping(segments_info):
     mapping = {0:-1}
@@ -82,22 +89,27 @@ def setup_cfg(args):
     cfg.freeze()
     return cfg
 
-def panoptic_segmentation():
-    # Perform Image Segmentation on the RGB Image    
+
+def load_model():
+    # Load Model for the Segmentation    
     mp.set_start_method("spawn", force=True)
-    args = argparse.Namespace(confidence_threshold=0.5, config_file=CONFIGS_DIR, input=[IMAGE_FILENAME], opts=['MODEL.WEIGHTS', MODEL_DIR], output='../data/outputs/', video_input=None, webcam=False)
+    global IMAGE_COUNTER
+    args = argparse.Namespace(confidence_threshold=0.5, config_file=CONFIGS_DIR, opts=['MODEL.WEIGHTS', MODEL_DIR], video_input=None, webcam=False)
     setup_logger(name="fvcore")
     logger = setup_logger()
     logger.info("Arguments: " + str(args))
     cfg = setup_cfg(args)
-
     demo = VisualizationDemo(cfg)
+    return demo, logger
 
-    if args.input:
-        if len(args.input) == 1:
-            args.input = glob.glob(os.path.expanduser(args.input[0]))
-            assert args.input, "The input path(s) was not found"
-        for path in tqdm.tqdm(args.input, disable=not args.output):
+
+def panoptic_segmentation(demo, logger, args_input, args_output):
+    # Perform Image Segmentation on the RGB Image
+    if args_input:
+        if len(args_input) == 1:
+            args_input = glob.glob(os.path.expanduser(args_input[0]))
+            assert args_input, "The input path(s) was not found"
+        for path in tqdm.tqdm(args_input, disable=not args_output):
             # use PIL, to be consistent with evaluation
             img = read_image(path, format="BGR")
             start_time = time.time()
@@ -113,13 +125,13 @@ def panoptic_segmentation():
             )
             
             # Saving output to Image, comment out for actual run on the agent
-            if args.output:
-                if os.path.isdir(args.output):
-                    assert os.path.isdir(args.output), args.output
-                    out_filename = os.path.join(args.output, os.path.basename(path))
+            if args_output:
+                if os.path.isdir(args_output):
+                    assert os.path.isdir(args_output), args_output
+                    out_filename = os.path.join(args_output, os.path.basename(path))
                 else:
-                    assert len(args.input) == 1, "Please specify a directory with args.output"
-                    out_filename = args.output
+                    assert len(args_input) == 1, "Please specify a directory with args.output"
+                    out_filename = args_output
                 visualized_output.save(out_filename)
             else:
                 cv2.namedWindow(WINDOW_NAME, cv2.WINDOW_NORMAL)
@@ -129,59 +141,104 @@ def panoptic_segmentation():
         
         return panoptic_seg, segments_info
 
-def publish_steer(steer = 0.0):
+
+def publish_steer(linearx, angularz):
     vel_msg = Twist()
-    vel_msg.angular.z = 0
+    vel_msg.linear.x = linearx
+    vel_msg.angular.z = angularz
     vel_pub.publish(vel_msg)
-    print(vel_msg)
+   # print(vel_msg)
+
 
 def callback(data):
+    print("Processing frame | Delay:%6.3f" % (rospy.Time.now() - data.header.stamp).to_sec())
+    delayed_secs = (rospy.Time.now() - data.header.stamp).to_sec()
+    if delayed_secs > 0.1:
+        return
     br = CvBridge()
     rospy.loginfo('Image received...')
     image = ros_numpy.numpify(data)
     image = image[:, :, ::-1]
     image = cv2.resize(image, (640, 480), interpolation = cv2.INTER_AREA)
-    cv2.imwrite(IMAGE_FILENAME, image)
+    #cv2.imshow("img", image)
+    #cv2.waitKey(10)
+    #rospy.sleep(1)
+    #return
+    global IMAGE_COUNTER
+
+    cv2.imwrite(IMAGE_PATH + str(IMAGE_COUNTER) + ".png", image)
     IM_WIDTH = 640
     IM_HEIGHT = 480
-    STEER_THRESHOLD = IM_WIDTH // 8
+    STEER_THRESHOLD = IM_WIDTH // 16
 
     # Perform Mask2Former Segmentation
-    panoptic_seg, segments_info = panoptic_segmentation()
+    global DEMO
+    global LOGGER
+    panoptic_seg, segments_info = panoptic_segmentation(DEMO, LOGGER, [IMAGE_PATH + str(IMAGE_COUNTER) + ".png"], '../data/outputs/m2f_' + str(IMAGE_COUNTER) + '.png')
 
     # Calculate traversable paths and areas
     _, traversable_paths = calculate_traversable_paths(panoptic_seg, segments_info)
+    
+    rgb_img = PILImage.open("../data/outputs/m2f_" + str(IMAGE_COUNTER) + ".png")
+    rgb_img_map = rgb_img.load()
+    rgb_img_traversable = PILImage.new(rgb_img.mode, rgb_img.size)
+    rgb_img_traversable_map = rgb_img_traversable.load()
+    for i in range(rgb_img.size[0]):
+        for j in range(rgb_img.size[1]):
+            rgb_img_traversable_map[i,j] = rgb_img_map[i,j]
 
     # Obtain a sample path
+    draw = ImageDraw.Draw(rgb_img_traversable)
     traversable_x = IM_WIDTH // 2
     traversable_y = IM_HEIGHT // 2
     for path in traversable_paths:
+        draw.ellipse((path['x'] - 2, path['y'] - 2, path['x'] + 2, path['y'] + 2), fill=(255, 0, 0))
         if path['y'] > (5 * IM_HEIGHT / 8) and path['y'] < (7 * IM_HEIGHT / 8):
             traversable_x = path['x']
             traversable_y = path['y']
-
+    
     dx = (traversable_x - (IM_WIDTH // 2))
-
+    steer = 0.0
     # Change steer based on dx
     print("Path found at X: " + str(traversable_x) + ", Y: " + str(traversable_y) + ".")
     dx = (traversable_x - (IM_WIDTH // 2))
     if dx > STEER_THRESHOLD:
-        steer = 0.5
+        steer = -0.5
         print("Action to be taken: Rotate Right.")
     elif dx < -STEER_THRESHOLD:
-        steer = -0.5
+        steer = 0.5
         print("Action to be taken: Rotate Left.")
     else:
         steer = 0.0
         print("Action to be taken: Move Forward.")
-        
-    publish_steer(steer)
+    
+    #rgb_img_traversable.save(RGBDP_PATH + "traversable_" + str(IMAGE_COUNTER) + ".png", "PNG")
+    img_cv2 = np.array(rgb_img_traversable)[:, :, 2::-1]
+    cv2.imshow("seg", img_cv2)
+    cv2.waitKey(10)
+
+    #rgb_img_traversable.show()
+    global ANGULARZ
+    ANGULARZ = steer
+    IMAGE_COUNTER += 1
 
 def listener():
     rospy.init_node('listener', anonymous=True)
-    rospy.Subscriber("/camera/color/image_raw", Image, callback)
-    rospy.spin()
+    global DEMO
+    global LOGGER
+    DEMO, LOGGER = load_model()
+    rospy.Subscriber("/camera/color/image_raw", Image, callback, queue_size=1, buff_size = 1000000000)
+    #rospy.spin()
+    rate = rospy.Rate(10)
+    global ANGULARZ
+    while not rospy.is_shutdown():
+        if ANGULARZ == 0.0:
+            publish_steer(0.4, ANGULARZ)
+        else:
+            publish_steer(0.0, ANGULARZ)
+        rate.sleep()
 
 if __name__ == '__main__':
-    vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+    #vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+    vel_pub = rospy.Publisher('/spot/cmd_vel', Twist, queue_size=10)
     listener()
